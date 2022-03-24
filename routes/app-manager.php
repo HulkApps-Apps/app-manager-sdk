46f3b3a05eaefe787410846966464d9073ca0201
Divyank <dvmunjapara@gmail.com>
2022-03-24 11:52:28 +0530

    Register route and controller

--- routes/app-manager.php
+++ routes/app-manager.php
@@ -1,15 +1,9 @@
 <?php
 
+use HulkApps\AppManager\app\Http\Controllers\BannerController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api/app-manager')->group(function () {
 
-    Route::get('marketing-banners', function ($type) {
-
-        if ($type === 'header') {
-            return "Return header data";
-        } else {
-            return "Footer data";
-        }
-    });
+    Route::get('marketing-banners', BannerController::class.'@index');
 });
