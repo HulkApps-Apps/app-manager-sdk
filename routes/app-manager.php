0680a7478aff585b6304efac9a4ce933ab8dffe9
Divyank <dvmunjapara@gmail.com>
2022-04-22 14:59:06 +0530

    Plan Featues

--- routes/app-manager.php
+++ routes/app-manager.php
@@ -1,11 +1,12 @@
 <?php
 
-use HulkApps\AppManager\app\Http\Controllers\FeaturesController;
+use HulkApps\AppManager\app\Http\Controllers\BannerController;
+use HulkApps\AppManager\app\Http\Controllers\PlanController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api/app-manager')->group(function () {
 
-    Route::get('marketing-banners', FeaturesController::class.'@index');
+    Route::get('marketing-banners', BannerController::class.'@index');
 
     Route::get('plan-features', PlanController::class.'@index');
 });
