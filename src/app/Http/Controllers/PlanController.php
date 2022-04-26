915be887fddc3730b7c02de12f9482123651c864
Chirag Parmar <chirag.p@hulkapps.com>
2022-04-26 19:11:56 +0530

    update

--- src/app/Http/Controllers/PlanController.php
+++ src/app/Http/Controllers/PlanController.php
@@ -2,7 +2,9 @@
 
 namespace HulkApps\AppManager\app\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 
 class PlanController extends Controller
 {
@@ -15,8 +17,30 @@ class PlanController extends Controller
 
     public function plans() {
 
+        $storeName = 'demo-anil.myshopify.com';
+
+        $tableName = config('app-manager.table_name', 'users');
+        $storeFieldName = config('app-manager.store_field_name', 'name');
+        $planFieldName = config('app-manager.plan_field_name', 'plan_id');
+
+        $activePlan = DB::table($tableName)->where($storeFieldName, $storeName)->pluck($planFieldName)->first();
+
         $plans = \AppManager::getPlans();
 
-        return response()->json(['plans' => $plans]);
+        $response = [
+            'plans' => $plans,
+            'active_plan_id' => $activePlan
+        ];
+
+        return response()->json($response);
+    }
+
+    public function planCharge(Request $request) {
+        $request->validate([
+            'charge_id' => 'required|number',
+            'plan_id' => 'required|number',
+            'user_id' => 'required|number',
+            'activated_on' => 'required',
+        ]);
     }
 }
