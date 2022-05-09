551c09b58485644d740ba6167a3371ee34c587a1
Chirag Parmar <chirag.p@hulkapps.com>
2022-05-09 16:23:34 +0530

    cache to plans and banners, update remaining-day helper function

--- src/app/Http/Controllers/BannerController.php
+++ src/app/Http/Controllers/BannerController.php
@@ -2,13 +2,20 @@
 
 namespace HulkApps\AppManager\app\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Cache;
 
 class BannerController extends Controller
 {
     public function index() {
 
-        $banners = \AppManager::getBanners();
+        $banners = Cache::get('banners', function () {
+            $response = \AppManager::getBanners();
+//			Cache::tags('app-manager-banners')->put('banners', $response, Carbon::now()->addDay());
+            Cache::put('banners', $response, Carbon::now()->addDay());
+            return $response;
+        });
 
         return response()->json(['banners' => $banners]);
     }
--- src/app/Http/Controllers/PlanController.php
+++ src/app/Http/Controllers/PlanController.php
@@ -5,6 +5,7 @@ namespace HulkApps\AppManager\app\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
@@ -19,30 +20,36 @@ class PlanController extends Controller
 
     public function plans(Request $request) {
 
-        $activePlanId = $shopify_plan = $plan = null;
         $shopTableName = config('app-manager.shop_table_name', 'users');
         $storeFieldName = config('app-manager.field_names.name', 'name');
         $planFieldName = config('app-manager.field_names.plan_id', 'plan_id');
         $shopifyPlanFieldName = config('app-manager.field_names.shopify_plan', 'shopify_plan');
 
-        $plans = \AppManager::getPlans();
+        $cacheKey = $request->has('shop_domain') ? 'plans-'.$request->get('shop_domain') : 'all-plans';
+        $response = Cache::get($cacheKey, function () use ($request, $shopTableName, $storeFieldName, $planFieldName, $shopifyPlanFieldName, $cacheKey) {
+            $shopify_plan = $plan = null;
+            $plans = \AppManager::getPlans();
 
-        if ($request->has('shop_domain')) {
-            $shopDomain = $request->get('shop_domain');
-            $userData = DB::table($shopTableName)->where($storeFieldName, $shopDomain)->get();
-            $shopify_plan = collect($userData)->pluck($shopifyPlanFieldName)->first();
-            $activePlanId = collect($userData)->pluck($planFieldName)->first();
-            $plan = collect($plans)->where('id', $activePlanId)->first();
-        }
+            if ($request->has('shop_domain')) {
+                $shopDomain = $request->get('shop_domain');
+                $userData = DB::table($shopTableName)->where($storeFieldName, $shopDomain)->get();
+                $shopify_plan = collect($userData)->pluck($shopifyPlanFieldName)->first();
+                $activePlanId = collect($userData)->pluck($planFieldName)->first();
+                $plan = collect($plans)->where('id', $activePlanId)->first();
+            }
 
-        $defaultPlanId = collect($plans)->where('interval', 'EVERY_30_DAYS')->sortByDesc('price')->pluck('id')->first();
+            $defaultPlanId = collect($plans)->where('interval', 'EVERY_30_DAYS')->sortByDesc('price')->pluck('id')->first();
 
-        $response = [
-            'plans' => $plans,
-            'shopify_plan' => $shopify_plan,
-            'plan' => $plan,
-            'default_plan_id' => $defaultPlanId
-        ];
+            $response = [
+                'plans' => $plans,
+                'shopify_plan' => $shopify_plan,
+                'plan' => $plan,
+                'default_plan_id' => $defaultPlanId
+            ];
+//			Cache::tags('app-manager-plans')->put($cacheKey, Carbon::now()->addDay());
+            Cache::put($cacheKey, $response, Carbon::now()->addDay());
+            return $response;
+        });
 
         return response()->json($response);
     }
@@ -85,8 +92,11 @@ class PlanController extends Controller
 
     public function burstCache(Request $request) {
         $type = $request->get('type');
-
-        // burst cache logic
-
+        if ($type === 'plans') {
+            Cache::tags('app-manager-plans')->flush();
+        }
+        elseif ($type === 'banners') {
+            Cache::tags('app-manager-banners')->flush();
+        }
     }
 }
